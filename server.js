605d0893a330ad3b6ffb5cc52238d54e59f87f81
ErikBoonprakong <e.boonprakong@gmail.com>
2022-02-28 11:14:40 +0000

    reconfigured cors

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const app = new Application();
 const PORT = parseInt(Deno.env.get("PORT")) || 80;
 
 const corsConfig = abcCors({
-  sameSite: "none",
+  sameSite: "None",
   // origin: process.env.REACT_APP_API_URL,
   // origin: "*",
   origin:
